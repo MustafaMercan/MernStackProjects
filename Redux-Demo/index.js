b4f04ef372ab2fab4b92e2bc6140c8ff74a32920
MustafaMercan <mustfmrcann@gmail.com>
2023-08-12 22:20:02 +0300

    redux-demo

--- Redux-Demo/index.js
+++ Redux-Demo/index.js
@@ -1,11 +1,15 @@
 
 const redux = require('redux');
+const reduxLogger = require('redux-logger')
 
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 
 const BUY_CAKE = 'BUY_CAKE';
-
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 
 // Defination Action
@@ -16,19 +20,49 @@ function buyCake()
         info:'First redux action'
     }
 }
+function buyIceCream()
+{
+    return{
+        type:BUY_ICECREAM,
+        info:'Second redux action'
+    }
+}
 
 
 
 // State
+/*
 const initialState = {
     numOfCakes : 10,
+    numOfIceCreams:20
+}*/
+
+const initialCakeState = {
+    numOfCakes:10,
 }
-
-
+const initialIceCreamState = {
+    numOfIceCreams:20,
+}
 // (previousState, action) => newState
 
+/*
 //reducer
 const reducer = (state = initialState , action) => {
+    switch(action.type){
+        case BUY_CAKE: return{
+            ...state,
+            numOfCakes: state.numOfCakes - 1,
+        }
+        case BUY_ICECREAM: return{
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1,
+        }
+        default: return state;
+    }
+}
+*/
+
+const cakeReducer = (state = initialCakeState , action) => {
     switch(action.type){
         case BUY_CAKE: return{
             ...state,
@@ -38,37 +72,37 @@ const reducer = (state = initialState , action) => {
     }
 }
 
+const iceCreamReducer = (state = initialIceCreamState , action) => {
+    switch(action.type){
+        case BUY_ICECREAM: return{
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1,
+        }
+        default: return state;
+    }
+}
 
-
-
-const store = createStore(reducer);
-console.log(store.getState());
-const unsubscribe =  store.subscribe(() => {
-    console.log('Updated store ', store.getState().numOfCakes);
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    iceCream:iceCreamReducer
 })
+const store = createStore(rootReducer,applyMiddleware(logger));
+
+console.log(store.getState());
+
+store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+
+store.dispatch(buyIceCream())
+
+
+
 store.dispatch(buyCake())
-
-unsubscribe();
-
-store.dispatch(buyCake())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
